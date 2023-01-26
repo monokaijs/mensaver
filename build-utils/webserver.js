b9628a4109cba5aca23b173bb21cff5817a94bdc
Edward Nguyen <monokaijs@gmail.com>
2023-01-26 20:49:31 +0700

    :sparkles: change webpack app name

--- build-utils/webserver.js
+++ build-utils/webserver.js
@@ -9,7 +9,7 @@ var WebpackDevServer = require('webpack-dev-server'),
   env = require('./env'),
   path = require('path');
 
-var options = config.chromeExtensionBoilerplate || {};
+var options = config.menSaverExtension || {};
 var excludeEntriesToHotReload = options.notHotReload || [];
 
 for (var entryName in config.entry) {
@@ -21,7 +21,7 @@ for (var entryName in config.entry) {
   }
 }
 
-delete config.chromeExtensionBoilerplate;
+delete config.menSaverExtension;
 
 var compiler = webpack(config);
 
