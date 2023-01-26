e83032c5ce5ae7d200a351a8b24e9527fdd9020e
Edward Nguyen <monokaijs@gmail.com>
2023-01-26 21:35:13 +0700

    :art: add options page's ui

--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -1,1 +1,9 @@
-console.log('loaded nicely!!');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Options from './Options';
+import './options.css';
+
+const container = document.getElementById('app-container');
+const root = createRoot(container as HTMLElement); // createRoot(container!) if you use TypeScript
+root.render(<Options/>);
