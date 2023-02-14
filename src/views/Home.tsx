0bd724f47908914ec74dbe8391e3ce4bdf4a10b5
MonokaiJs <monokaijs@gmail.com>
2023-02-14 11:50:58 +0700

    🎨 add settings route #3

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import {Card, Col, Row} from "antd";
+
+const AnalyticsCard = () => {
+  return (
+    <Card
+      bordered={false}
+      style={{marginBottom: 16}}
+    >
+      <h2>
+        123
+      </h2>
+    </Card>
+  )
+}
 
 export default function Home() {
   return (
     <div style={{marginTop: 32}}>
-      Home is here!
+      <Row gutter={16}>
+        <Col sm={12} md={6}>
+          <AnalyticsCard/>
+        </Col>
+        <Col sm={12} md={6}>
+          <AnalyticsCard/>
+        </Col>
+        <Col sm={12} md={6}>
+          <AnalyticsCard/>
+        </Col>
+        <Col sm={12} md={6}>
+          <AnalyticsCard/>
+        </Col>
+      </Row>
     </div>
   )
 }
---
+++ src/views/Settings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Settings() {
+  return (
+    <div className={'page'}>
+      <h1>
+        Settings
+      </h1>
+    </div>
+  )
+}
