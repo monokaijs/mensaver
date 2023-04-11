0c407582ba7fc30f9a278abe10d23f4d579eda23
MonokaiJs <monokaijs@gmail.com>
2023-04-12 03:48:02 +0700

    🚧 auto hide post based on gender

--- src/extraScripts/injected_script.js
+++ src/extraScripts/injected_script.js
@@ -20,7 +20,6 @@ function patch(moduleName, context, args, patchFunction, exported = "default") {
 console.log('works');
 
 const getProfileGender = (userId) => {
-  console.log('get-profile-gender', userId);
   return new Promise((resolve, reject) => {
     getUserMetaInfo(userId).then(data => {
       if (data && data.node && data.node['comet_hovercard_renderer'] && data.node['comet_hovercard_renderer'].user) {
@@ -29,7 +28,6 @@ const getProfileGender = (userId) => {
         const friendAction = primaryActions.find(x => x['__typename'] === 'ProfileActionFriendRequest');
         const profileAction = friendAction?.client_handler?.profile_action;
         const profileOwner = profileAction?.restrictable_profile_owner;
-        console.log('profile-owner', profileOwner);
         if (profileOwner) {
           resolve(profileOwner.gender);
         } else {
@@ -75,6 +73,31 @@ Object.defineProperty(window, "__d", {
           }, []);
           return SmartCover(gender, originalComponent);
         });
+        patch('CometFeedStory.react', this, arguments, function (orgFunc, that, args) {
+          const react = window.require('react');
+          const originalComponent = orgFunc.bind(that)(...args);
+          const [gender, setGender] = react.useState('');
+          const props = args[0];
+          const storyId = props?.story.id;
+          let authorId;
+          react.useEffect(() => {
+            if (!storyId) return;
+            (async () => {
+              const decodedStoryId = atob(storyId);
+              console.log('decoded', decodedStoryId);
+              if (decodedStoryId.startsWith('S:_I') && !decodedStoryId.includes('/')) {
+                authorId = decodedStoryId.replace('S:_I', '').split(':')[0];
+              } else {
+                const account = await getPostAuthor(storyId);
+                authorId = account.id;
+              }
+              const userGender = await getProfileGender(authorId);
+              console.log('gender', userGender);
+              setGender(userGender);
+            })();
+          }, []);
+          return SmartCover(gender, originalComponent, true);
+        });
         return __dOrig.apply(this, arguments);
       } catch (e) {
         console.log("Patching Error", e);
@@ -85,7 +108,7 @@ Object.defineProperty(window, "__d", {
   },
 });
 
-const SmartCover = (gender, children) => {
+const SmartCover = (gender, children, isPost = false) => {
   const react = window.require('react');
   const [cover, setCover] = react.useState(true);
   if (gender === 'MALE') return children;
@@ -94,7 +117,7 @@ const SmartCover = (gender, children) => {
     children: [
       children,
       cover ? react.jsx('div', {
-        className: 'smart-cover',
+        className: isPost ? 'smart-cover post': 'smart-cover',
         children: [
           react.jsx('div', {
             className: 'message',
@@ -131,6 +154,43 @@ const getMutualFriends = (uid) => {
     "ordering": ["importance"],
     "userIDs": [uid]
   })
+}
+
+const getPostAuthor = async (storyId) => {
+  const response = await graphQl('5874694995986171', {
+    "UFI2CommentsProvider_commentsKey": "CometSinglePostRoute",
+    "displayCommentsContextEnableComment": null,
+    "displayCommentsContextIsAdPreview": null,
+    "displayCommentsContextIsAggregatedShare": null,
+    "displayCommentsContextIsStorySet": null,
+    "displayCommentsFeedbackContext": null,
+    "feedbackSource": 2,
+    "feedLocation": "PERMALINK",
+    "focusCommentID": null,
+    "hasScenario": false,
+    "privacySelectorRenderLocation": "COMET_STREAM",
+    "renderLocation": "permalink",
+    "scale": 1.5,
+    "storyID": storyId,
+    "useDefaultActor": false,
+    "__relay_internal__pv__GroupsCometDelayCheckBlockedUsersrelayprovider": false,
+    "__relay_internal__pv__IsWorkUserrelayprovider": false,
+    "__relay_internal__pv__IsMergQAPollsrelayprovider": false,
+    "__relay_internal__pv__StoriesArmadilloReplyEnabledrelayprovider": false,
+    "__relay_internal__pv__StoriesRingrelayprovider": false
+  }).then(r => r.text());
+  if (response && response.split('\n')[0]) {
+    try {
+      const storyData = JSON.parse(response.split('\n')[0]);
+      const storyActors = storyData?.data?.node?.comet_sections?.content?.story?.actors || [];
+      if (storyActors.length === 0) throw new Error("Failed to get actors");
+      return storyActors[0];
+    } catch (e) {
+      throw new Error(e.message || "Failed to parse story information");
+    }
+  } else {
+    throw new Error("Failed to parse");
+  }
 }
 
 const graphQl = (docId, variables) => {
@@ -180,6 +240,7 @@ window.document.addEventListener('chrome-message', e => {
     __menSaverAppSettings = event.settings;
   }
 });
+
 function addChromeMessageListener(action, fn) {
   chromeMessageListeners[action] = chromeMessageListeners[action] ? [
     ...chromeMessageListeners[action],
